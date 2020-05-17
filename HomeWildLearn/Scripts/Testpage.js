db9b732e554458c6ceb6667dfdc13adc53dca8fa
ishan <ishan@DESKTOP-LDOG3I6>
2020-05-17 11:30:11 +1000

    leankit changes

---
+++ HomeWildLearn/Scripts/Testpage.js
@@ -0,0 +1,8 @@
+﻿ Draggable.create(".icon", {
+        bounds: "svg",
+        onDrag: function () {
+            if (this.hitTest("#bag")) {
+                gsap.to(this.target, { duration: 0.6, opacity: 0, scale: 0, svgOrigin: "675 143" });
+            }
+        }
+    });
